4de2ee696e5839a0c7a6cf3854ddf43aab0afea3
Jeff <jeff@unbiasedgeek.com>
2024-06-05 14:02:40 -0600

    Switch from Packer to Lazy in NeoVim

--- nvim/lua/techjeff/init.lua
+++ nvim/lua/techjeff/init.lua
@@ -1,2 +1,97 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+
+require('lazy').setup({
+  'mbbill/undotree',  
+  'tpope/vim-fugitive',
+  'xero/miasma.nvim',
+  {
+      'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate',
+  },
+  {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+	  -- or                            , branch = '0.1.x',
+	  dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
+  {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  dependencies = {
+		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+		  {'neovim/nvim-lspconfig'},
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'L3MON4D3/LuaSnip'},
+	  }
+  },
+  {
+    "rebelot/kanagawa.nvim", -- neorg needs a colorscheme with treesitter support
+    config = function()
+        vim.cmd.colorscheme("kanagawa")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+      highlight = { enable = true },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  {
+      "vhyrro/luarocks.nvim",
+      priority = 1000,
+      config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    version = "*",
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  }
+
+})
+
+vim.cmd("colorscheme miasma")
+
+
 require("techjeff.remap")
 require("techjeff.set")
